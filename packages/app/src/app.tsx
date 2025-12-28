e58b0036781084ce7369ced52b02d3b4833fa25a
James Garbutt <43081j@users.noreply.github.com>
2025-12-28 20:16:58 +0000

    fix: banish overflow (#7)

--- packages/app/src/app.tsx
+++ packages/app/src/app.tsx
@@ -5,7 +5,7 @@ export function App() {
   return (
     <div class="relative md:flex h-screen">
       <Sidebar />
-      <main class="flex-1 bg-gray-50 w-full md:w-auto">
+      <main class="flex-1 bg-gray-50 w-full md:w-auto h-screen overflow-hidden">
         <ContentPanel />
       </main>
     </div>
